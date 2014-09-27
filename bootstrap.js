460318bb25ff2345a0e2f86acc0d268c099f6104
Jelle De Loecker <jelle@codedor.be>
2014-09-27 20:56:25 +0200

    Setup support for v0.2

--- bootstrap.js
+++ bootstrap.js
@@ -1,39 +1,6 @@
-// Send the menu options to the client
-alchemy.on('alchemy.render', function(settings, callback) {
+// Tell each ViewRender instance to load jQuery
+alchemy.hawkejs.on({type: 'viewrender', status: 'begin', client: false}, function onBegin(viewRender) {
 
-	// Only send this data on the initial pageload
-	if (!settings.ajax) {
-
-		var path = '/public/scripts/jquery.js',
-		    cdn  = '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js';
-
-		if (!settings.payload.request) {
-			settings.payload.request = {};
-		}
-
-		if (!settings.payload.request.tags) {
-			settings.payload.request.tags ={};
-		}
-
-		// Add the CDN script
-		settings.payload.request.tags[cdn] = {
-			type: 'script',
-			path: cdn,
-			block: 'head',
-			order: 1001,
-			suborder: 1000
-		};
-
-		// Add the local fallback script
-		settings.payload.request.tags[path] = {
-			type: 'script',
-			code: "window.jQuery || document.write('<scr' + 'ipt src=\"" + path + "\"><\/scr' + 'ipt>')",
-			block: 'head',
-			force: true,
-			order: 1000,
-			suborder: 1000
-		};
-	}
-
-	callback();
+	// @todo: add support for local fallback
+	viewRender.script('//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
 });
